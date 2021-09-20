c98501d076d3253ea5c14f3e3eace8586102f941
anji014 <82080207+anji014@users.noreply.github.com>
2021-09-20 11:37:59 +0530

    Update App.java

--- src/main/java/jenkins-project1/App.java
+++ src/main/java/jenkins-project1/App.java
@@ -7,7 +7,7 @@ public class App {
 
     for (int i = 1; i <= rows; ++i) {
       for (int j = 1; j <= i; ++j) {
-        System.out.print("* ");
+        System.out.print(" * ");
       }
       System.out.println();
     }
